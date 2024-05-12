77185744a8d2892bf49b0b80485475ab787a96cc
Jun Nishimura <n.junjun0303@gmail.com>
2024-05-12 17:22:46 +0900

    finish until 3.5.2

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/JunNishimura/Monkey/evaluator"
 	"github.com/JunNishimura/Monkey/lexer"
 	"github.com/JunNishimura/Monkey/parser"
 )
@@ -32,8 +33,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, program.String())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
